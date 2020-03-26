af46d052d97f1447d737f26d800de81770e6bbb3
gdormoy <dormoy.guillaume@gmail.com>
2020-03-26 09:26:02 -0700

    remove connectore file caus not a class

--- tools/mysql/connector.py
+++ tools/mysql/connector.py
@@ -1,20 +1,2 @@
-import sys
-import logging
 
-import MySQLdb
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-def create_connection(hostname, username, password, database):
-    try:
-        db = MySQLdb.connect(hostname, user=username,
-                               passwd=password, db=database, connect_timeout=10)
-    except:
-        logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
-        sys.exit()
-
-    logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
-    return db
 
