f7f8e695a80e0675deb72409bca48988bee3ccc8
gdormoy <dormoy.guillaume@gmail.com>
2020-03-26 10:30:51 -0700

    missing 's'

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gdormoy/lambda-toolbox",
-    package=setuptools.find_packages(),
+    packages=setuptools.find_packages(),
     install_requires=libs,
     classifiers=[
         "Programming Language :: Python :: 3",
